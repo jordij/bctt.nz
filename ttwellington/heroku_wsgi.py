31d473d78e3409ad89746881b1af1ae2fd9a804f
jordijoan <jordi.joan@gmail.com>
2015-08-25 08:51:08 +1200

    wsgi

--- ttwellington/heroku_wsgi.py
+++ ttwellington/heroku_wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ttwellington.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ttwellington.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
