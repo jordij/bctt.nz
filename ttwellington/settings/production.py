e44721bb15246500ba29fdc4c5afb3b6b89cfbe5
Jordi Joan <jordi.joan@gmail.com>
2018-03-08 06:51:43 +0000

    SSL flag.

--- ttwellington/settings/production.py
+++ ttwellington/settings/production.py
@@ -28,6 +28,7 @@ RECAPTCHA_PRIVATE_KEY = env['RECAPTCHA_PRIVATE_KEY']
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 # Allow domain host headers
 ALLOWED_HOSTS = [
